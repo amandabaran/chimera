fd9df88eb930fae8f95e4049820d4190057f53db
Amanda Baran <adb321lehigh.edu>
2026-05-06 16:50:37 -0400

    Fix: Ignore Wignored-attributes for GCC 13 compatibility

---
+++ chimera/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Chimera protocol initialized!" << std::endl;
+    return 0;
+}
--- fusee/src/main.hpp
+++ fusee/src/main.hpp
@@ -1,3 +1,4 @@
+#pragma GCC diagnostic ignored "-Wignored-attributes"
 #pragma once
 
 #include <chrono>
